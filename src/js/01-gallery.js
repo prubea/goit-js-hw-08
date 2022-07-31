8c6918865f6976c02d977921a264964bbce23051
Beata Pruszczyk <prubea@gmail.com>
2022-08-01 01:40:52 +0200

    01-gallery
    Zadanie1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,9 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector("div.gallery");
 
