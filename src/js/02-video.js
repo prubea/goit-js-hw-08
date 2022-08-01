88b9e38c9d528a6ee73e9251eadff766ae88b106
Beata Pruszczyk <prubea@gmail.com>
2022-08-01 14:50:47 +0200

    02-video
    Zadanie2
    Dodanie biblioteki poprzez npm:
    npm install @vimeo/player
    npm install lodash.throttle
    npm install vimeo
    - Śledzenie zdarzenia timeupdate - aktualizacja czasu odtwarzania
    - Zapisanie czasu odtwarzania w local storage
    - Po przeładowywaniu strony wznowienie odtwarzania od zapisanego momentu
    - czas odtwarzania aktualizuje się w storage nie częściej niż raz na sekundę

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(({ seconds }) => {
+    localStorage.setItem("videoplayer-current-time", `${seconds}`);
+    
+}, 1000));
+
+const actualTime = localStorage.getItem("videoplayer-current-time");
+
+player.setCurrentTime(actualTime).then(function (seconds) {
+        // seconds = the actual time that the player seeked to
+    }).catch(function (error) {
+        switch (error.name) {
+            case 'RangeError':
+                // the time was less than 0 or greater than the video’s duration
+                break;
+
+            default:
+                // some other error occurred
+                break;
+        }
+    });
+
+player.setVolume(0);
+
+player.on('play', function() {
+    console.log('played the video!');
+});
