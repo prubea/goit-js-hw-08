64a67cd8f985dc2c7f511c860eb6275d7936639e
Beata Pruszczyk <prubea@gmail.com>
2022-08-02 00:35:20 +0200

    Update 03-feedback.js
    Zadanie 3
    - zapisywanie w local storage obiekt z polami email i message z aktualnymi danymi
    - wypełnianie zapisanymi danymi pola formularza podczas przeładowywania strony, gdy w storage są dane zapisane

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector("form.feedback-form");
+const input = document.querySelector("input");
+const textArea = document.querySelector("textarea");
+
+form.addEventListener("input", throttle((event) => {
+      const objectInput = { email: form.elements.email.value, message: form.elements.message.value };
+      localStorage.setItem("feedback-form-state", JSON.stringify(objectInput));
+      
+}, 500));
+
+form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      const { elements: { email, message } } = event.currentTarget;
+      console.log({ email: email.value, message: message.value });
+      
+      event.currentTarget.reset();
+      localStorage.clear();
+});
+
+const storageData = localStorage.getItem("feedback-form-state");
+const storageDataParse = JSON.parse(storageData);
+const took = () => {
+    if (storageDataParse !== null) {
+        input.value = storageDataParse.email;
+        textArea.value = storageDataParse.message;
+    };
+};
+
+took();
